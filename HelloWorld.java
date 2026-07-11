8a5ac486f2105743510fb0a266519297575f7beb
vemula2448 <vemulapraveen2448@gmail.com>
2026-07-11 15:48:11 +0530

    Add HelloWorld class with main method
    git hub testing

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+        System.out.println("This is my first Java program.");
+        System.out.println("Testing Git Push.");
+    }
+}
